1484564eb692e0bafdd7b6a20208258457d11b57
Omerta85 <666Omerta@gmail.com>
2022-09-20 18:21:24 +0300

    prew lesson 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
 import './App.css';
-import Users from "./components/users/Users";
+import {createRef, useState} from "react";
+
 
 function App() {
+    //контрольований компонент
+    // let [login,setLogin] = useState('default');
+    // let [password, setPassword] = useState('');
+    // function onLoginChange(e) {
+    //     setLogin(e.target.value);
+    // }
+    // function onPasswordChange(e) {
+    //     setPassword(e.target.value);
+    // }
+  //   let [formState, setFormState] = useState({login:'', password:''});
+  //
+  //   function onSubmit(e) {
+  //       console.log(e.target.login.value);
+  //       console.log(e.target.password.value);
+  //       e.preventDefault();
+  //       console.log(formState);
+  //   }
+  //  function onChange (e){
+  //       setFormState({...formState, [e.target.name]:e.target.value})
+  //   }
+  // return (<div>
+  //     <form onSubmit={onSubmit}>
+  //         <input type="text" name={'login'} value={formState.login} onChange={onChange}/>
+  //         <input type="text" name={'password'} value={formState.password} onChange={onChange}/>
+  //         <button>submit</button>
+  //     </form>
+  // </div>);
 
-  return (<div>
-        <h2>app component header</h2>
-        <Users/>
-  </div>);
+    //не контрольований компонент
+    let login = createRef();
+    let password = createRef();
+    function onSubmit(e) {
+        console.log(login.current.value);
+        console.log(password.current.value);
+        e.preventDefault();
+    }
+    return (<div>
+           <form onSubmit={onSubmit}>
+                <input type="text" name={'login'} ref={login}/>
+              <input type="text" name={'password'} ref={password}/>
+                <button>log in</button>
+          </form>
+     </div>);
+
 }
 
 export default App;
